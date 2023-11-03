8f821dd49ceb865fccb28a1f385ad0af46191b56
Joseph Beloved <belovedtoluwanimi16@gmail.com>
2023-11-03 23:45:34 +0100

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.svg'
+import tick from './tick.svg'
+import purp from './purp_Grove.png'
+import grove from './grove.png'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    tick,
+    purp,
+    grove,
+}
